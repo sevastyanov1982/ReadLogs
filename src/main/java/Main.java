51103349671c23691b27da3dc1bbc39b4eb3514e
sevastyanov1982 <sevastyanov1982@gmail.com>
2021-03-16 22:00:39 +0300

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,10 @@ public class Main {
     long amount = 1000_60;
     boolean registered = true;
 
-    service.calculate(amount, registered);
+    long bonusBelowLimitRegistered = service.calculate(amount, registered);
+    System.out.println(bonusBelowLimitRegistered);
+
+
   }
 }
 
